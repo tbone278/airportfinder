6d2cb2edf7ac4435eed0bd19860704a16b0edd25
tbone278 <tbone278@googlemail.com>
2026-06-30 06:22:50 +0200

    Rename utils.js to js/utils.js

--- utils.js
+++
@@ -1,2 +0,0 @@
-export const openAppleMaps=(lat,lon)=>window.open(`https://maps.apple.com/?ll=${lat},${lon}`,'_blank');
-export const openGoogleMaps=(lat,lon)=>window.open(`https://www.google.com/maps?q=${lat},${lon}`,'_blank');
